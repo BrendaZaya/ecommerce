4c74238abcbd6d8127962a28a3d7e0da935dbc80
brenda zaya <brendazayait@gmail.com>
2022-06-29 02:03:02 -0300

    correcion de arrays

--- index.js
+++ index.js
@@ -13,197 +13,172 @@ function validarEdad() {
     const productos = [
         {
             "id": 1,
-            "img": "IPA.png",
             "nombre": "IPA",
-            "color": "6  SRM",
-            "amargor": "43 IBUS",
+            "amargor": "ALTO",
+            "ibu": "82",
             "aroma": "CITRICA",
-            "alcohol": "4,9°",
-            "tamaño": "1 litro",
-            "precio": 400
+            "ABV": "6,5%",
+            "tamaño": "500 cc",
+            "precio": 260
         },
         
         {
             "id": 2,
-            "img": "APA.png",
-            "nombre": "APA",
-            "color": "11 SRM",
-            "amargor": "45 IBUS",
-            "aroma": "CITRICA",
-            "alcohol": "5.7°",
-            "tamaño": "1 litro",
-            "precio": 400
+            "nombre": "KOLSCH",
+            "amargor": "MODERADO",
+            "ibu": "21",
+            "aroma": "FRUTADA",
+            "ABV": "4,5%",
+            "tamaño": "500 cc",
+            "precio": 220
         },
         
         {
             "id": 3,
-            "img": "STOUT.png",
             "nombre": "STOUT",
-            "color": "40 SRM",
-            "amargor": "28 IBUS",
-            "aroma": "TOSTADO",
-            "alcohol": "5.3°",
-            "tamaño": "1 litro",
-            "precio": 400
+            "amargor": "MEDIO",
+            "ibu": "42",
+            "aroma": "TOSTADA",
+            "ABV": "6,5%",
+            "tamaño": "500 cc",
+            "precio": 240
         },
         
         {
             "id": 4,
-            "img": "SESION.png",
-            "nombre": "SESSION IPA",
-            "color": "8 SRM",
-            "amargor": "40 IBUS",
+            "nombre": "RED IPA",
+            "amargor": "ALTO",
+            "ibu": "70",
             "aroma": "CITRICA",
-            "alcohol": "4°",
-            "tamaño": "1 litro",
-            "precio": 400
+            "ABV": "6,0%",
+            "tamaño": "500 cc",
+            "precio": 260
         },
         
         {
             "id": 5,
-            "img": "NEIPA.png",
-            "nombre": "NEIPA",
-            "color": "10 SRM",
-            "amargor": "32 IBUS",
-            "aroma": "FRUTADA",
-            "alcohol": "5°",
-            "tamaño": "1 litro",
-            "precio": 400
+            "nombre": "BARLEY WINE",
+            "amargor": "MEDIO",
+            "ibu": "54",
+            "aroma": "CARAMELO",
+            "ABV": "11%",
+            "tamaño": "500 cc",
+            "precio": 240
         },
 
         {
             "id": 6,
-            "img": "honey2.png",
-            "nombre": "HONEY",
-            "color": "6  SRM",
-            "amargor": "14 IBUS",
-            "aroma": "MALTA",
-            "alcohol": "6°",
-            "tamaño": "1 litro",
-            "precio": 400
+            "nombre": "PORTER",
+            "amargor": "BAJO",
+            "ibu": "32",
+            "aroma": "CHOCOLATE",
+            "ABV": "6,5%",
+            "tamaño": "500 cc",
+            "precio": 240
         },
 
         {
             "id": 7,
-            "img": "redhoney.png",
-            "nombre": "RED HONEY",
-            "color": "18  SRM",
-            "amargor": "14 IBUS",
-            "aroma": "MALTA",
-            "alcohol": "6.8°",
-            "tamaño": "1 litro",
-            "precio": 400
+            "nombre": "HONEY",
+            "amargor": "BAJO",
+            "ibu": "20",
+            "aroma": "CITRICA",
+            "ABV": "6%",
+            "tamaño": "500 cc",
+            "precio": 220
         },
 
         {
             "id": 8,
-            "img": "scotish.png",
-            "nombre": "SCOTISH",
-            "color": "18  SRM",
-            "amargor": "18 IBUS",
-            "aroma": "MALTA",
-            "alcohol": "6.3°",
-            "tamaño": "1 litro",
-            "precio": 400
+            "nombre": "RED HONEY",
+            "amargor": "BAJO",
+            "ibu": "15",
+            "aroma": "ACARAMELADO",
+            "ABV": "4,5%",
+            "tamaño": "500 cc",
+            "precio": 220
         },
         
         {
             "id": 9,
-            "img": "IPA.png",
-            "nombre": "IPA",
-            "color": "6  SRM",
-            "amargor": "43 IBUS",
-            "aroma": "CITRICA",
-            "alcohol": "4,9°",
-            "tamaño": "2 litros",
-            "precio": 700
+            "nombre": "IRISH",
+            "amargor": "MEDIO",
+            "ibu": "24",
+            "aroma": "MALTA",
+            "ABV": "5,3%",
+            "tamaño": "500 cc",
+            "precio": 220
         },
         
-            {
+        {
             "id": 10,
-            "img": "APA.png",
-            "nombre": "APA",
-            "color": "11 SRM",
-            "amargor": "45 IBUS",
-            "aroma": "CITRICA",
-            "alcohol": "5.7°",
-            "tamaño": "2 litros",
-            "precio": 700
+            "nombre": "GOLDEN ALE",
+            "amargor": "BAJO",
+            "ibu": "22",
+            "aroma": "FRUTAL",
+            "ABV": "5%",
+            "tamaño": "500 cc",
+            "precio": 220
         },
         
         {
             "id": 11,
-            "img": "STOUT.png",
-            "nombre": "STOUT",
-            "color": "40 SRM",
-            "amargor": "28 IBUS",
-            "aroma": "TOSTADO",
-            "alcohol": "5.3°",
-            "tamaño": "2 litros",
-            "precio": 700
+            "nombre": "APA",
+            "amargor": "MEDIO",
+            "ibu": "34",
+            "aroma": "LÚPULO",
+            "ABV": "5,5%",
+            "tamaño": "500 cc",
+            "precio": 240
         },
         
         {
             "id": 12,
-            "img": "SESION.png",
-            "nombre": "SESSION IPA",
-            "color": "8 SRM",
-            "amargor": "40 IBUS",
-            "aroma": "CITRICA",
-            "alcohol": "4°",
-            "tamaño": "2 litros",
-            "precio": 700
+            "nombre": "CREAM STOUT",
+            "amargor": "MODERADO",
+            "ibu": "32",
+            "aroma": "CAFÉ",
+            "ABV": "8%",
+            "tamaño": "500 cc",
+            "precio": 260
         },
         
         {
             "id": 13,
-            "img": "NEIPA.png",
             "nombre": "NEIPA",
-            "color": "10 SRM",
-            "amargor": "32 IBUS",
-            "aroma": "FRUTADA",
-            "alcohol": "5°",
-            "tamaño": "2 litros",
-            "precio": 700
+            "amargor": "ALTO",
+            "ibu": "62",
+            "aroma": "LÚPULO",
+            "ABV": "8,5%",
+            "tamaño": "500 cc",
+            "precio": 260
         },
 
         {
             "id": 14,
-            "img": "honey2.png",
-            "nombre": "Honey",
-            "color": "6  SRM",
-            "amargor": "14 IBUS",
-            "aroma": "MALTA",
-            "alcohol": "6°",
-            "tamaño": "2 litros",
-            "precio": 700
+            "nombre": "AMERICAN STOUT",
+            "amargor": "MEDIO",
+            "ibu": "42",
+            "aroma": "CANELA",
+            "ABV": "6%",
+            "tamaño": "500 cc",
+            "precio": 240
         },
 
         {
             "id": 15,
-            "img": "redhoney.png",
-            "nombre": "RED HONEY",
-            "color": "18  SRM",
-            "amargor": "14 IBUS",
+            "nombre": "PILSEN",
+            "amargor": "BAJO",
+            "ibu": "18",
             "aroma": "MALTA",
-            "alcohol": "6.8°",
-            "tamaño": "2 litros",
-            "precio": 700
-        },
-
-        {
-            "id": 16,
-            "img": "scotish.png",
-            "nombre": "SCOTISH",
-            "color": "18  SRM",
-            "amargor": "18 IBUS",
-            "aroma": "MALTA",
-            "alcohol": "6.3°",
-            "tamaño": "2 litros",
-            "precio": 700
+            "ABV": "5%",
+            "tamaño": "500 cc",
+            "precio": 220
         }
     ]
     
+    // precio total de todos los productos
+
     const total = productos.reduce((acumulador, elemento) => acumulador + elemento.precio, 0);
     console.log(total);
 
